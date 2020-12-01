d3cec87abba1ce7abd6ad6381818768dc4346c2e
Chirag Chaplot <chiragchaplot@gmail.com>
2020-12-02 00:15:00 +1100

    Separate main thread tasks and async thread tasks

--- Bourne Digital Test/ViewController.swift
+++ Bourne Digital Test/ViewController.swift
@@ -28,6 +28,7 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         
     }
     
+    //Start to get the JSON
     private func startLoading()
     {
         let urlString = "https://www.dropbox.com/s/q1ins5dsldsojzt/movies.json?dl=1"
@@ -35,17 +36,14 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         self.loadJson(fromURLString: urlString) { (result) in
             switch result {
             case .success(let data):
-                self.parse(jsonData: data)
                 //Fill up the tables
-                self.tableView.delegate = self
-                self.tableView.dataSource = self
-                //self.tableView.reloadData()
+                self.parse(jsonData: data)
             case .failure(let error):
                 print(error)
             }
         }
     }
-            
+            //Load the actual JSON
         private func loadJson(fromURLString urlString: String,
                               completion: @escaping (Result<Data, Error>) -> Void) {
             if let url = URL(string: urlString) {
@@ -55,6 +53,10 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     }
                     
                     if let data = data {
+                        DispatchQueue.main.async {
+                            self.tableView.delegate = self
+                            self.tableView.dataSource = self
+                        }
                         completion(.success(data))
                     }
                 }
@@ -63,10 +65,13 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
             }
         }
 
+    //Parse the json
         private func parse(jsonData: Data) {
             do {
                 self.mainObject = try JSONDecoder().decode(MainObject.self, from: jsonData)
                 
+                //Check logs only on simulator
+                #if targetEnvironment(simulator)
                 print("Title: ", self.mainObject.title)
                 
                 for movie in self.mainObject.movies
@@ -77,23 +82,28 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
                     print("Release Date",movie.releaseDate)
                     print("===================================")
                 }
-                
-                self.tableView.reloadData()
-                
+                #endif
+                //Call the main thread to reload data
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
                 
             } catch {
                 print("parse:- " + error.localizedDescription)
             }
         }
     
+    //get number of rows in table
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int{
         return mainObject.movies.count
     }
     
+    //Set height of row manually
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
     }
     
+    //Custom Cell work
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
     {
         let cell = tableView.dequeueReusableCell(withIdentifier: "customCell") as! CustomTableViewCell
@@ -103,16 +113,18 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         cell.movieImage.sd_setImage(with: URL(string: cell.movieImageURL ?? ""),
                                       placeholderImage: UIImage(named: "placeholder.png"
                                      ))
-        
+        //Curved Corners
         cell.cellView.layer.cornerRadius = cell.cellView.frame.height/2
         cell.movieImage.layer.cornerRadius = cell.movieImage.frame.height/2
         return cell
     }
     
+    //Cell Click
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         performSegue(withIdentifier: "showDetails", sender: self)
     }
     
+    //Segue Operation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if let destination = segue.destination as? MovieViewController{
             destination.movie = mainObject.movies[tableView.indexPathForSelectedRow!.row]
